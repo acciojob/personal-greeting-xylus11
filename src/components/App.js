eec3ae2568b960595ecdb0a76cd7a5c65179f59e
mangeshahirayy <97844580+xylus11@users.noreply.github.com>
2024-02-25 19:15:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue("Hello " + e.target.value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input type="text" id="inp" onChange={handleInputChange} />
+      <span>{inputValue}</span>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
