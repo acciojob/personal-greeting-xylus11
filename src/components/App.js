100dfe8b7fae47b3bb877d2ed34e31c17953bed9
mangeshahirayy <97844580+xylus11@users.noreply.github.com>
2024-02-25 19:21:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ const App = () => {
   const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (e) => {
-    setInputValue("Hello " + e.target.value);
+    setInputValue("Hello " + e.target.value +"!");
   };
 
   return (
     <div>
       <input type="text" id="inp" onChange={handleInputChange} />
-      <p>{inputValue}</p>
+      <p>{inputValue} </p>
     </div>
   );
 };
